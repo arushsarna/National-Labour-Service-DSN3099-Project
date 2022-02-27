aaae5813e6dd5d6c7cab7a277ed7067413bf6d48
arushsarna2019 <96369317+arushsarna2019@users.noreply.github.com>
2022-02-27 17:22:28 +0530

    Making a realtime database with firebase
    making a working form

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -5,24 +5,83 @@ import Button from '@material-tailwind/react/Button';
 import Input from '@material-tailwind/react/Input';
 import Textarea from '@material-tailwind/react/Textarea';
 
+import { useState } from 'react';
+
+
+
 export default function SettingsForm() {
+    const [user, setUser] = useState(
+        {
+            name: " ",
+            firstname: " ",
+            lastname: " ",
+            email: " ",
+            address: " ",
+            city: " ",
+            country: " ",
+            postalcode: " ",
+            aboutme: "  ",
+
+
+        });
+    let name, value;
+    const getUserData = (event) => {
+        name = event.target.name;
+        value = event.target.value;
+        setUser({ ...user, [name]: value });
+
+    };
+    const postData = async (e) => {
+        e.preventDefault();
+        const { name, firstname, lastname, email, address, city, country, postalcode, aboutme } = user;
+        const res = await fetch(
+            "https://national-labour-service-dsn-default-rtdb.firebaseio.com/userdata.json",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(
+                    {
+                        name,
+                        firstname,
+                        lastname,
+                        email,
+                        address,
+                        city,
+                        country,
+                        postalcode,
+                        aboutme,
+
+                    }),
+            }
+        );
+        if (res) {
+            setUser({
+                name: " ",
+                firstname: " ",
+                lastname: " ",
+                email: " ",
+                address: " ",
+                city: " ",
+                country: " ",
+                postalcode: " ",
+                aboutme: " ",
+
+            });
+            alert("Dara stored")
+        }
+    };
     return (
         <Card>
             <CardHeader color="purple" contentPosition="none">
                 <div className="w-full flex items-center justify-between">
                     <h2 className="text-white text-2xl">My Account</h2>
-                    {/* <Button
-                        color="transparent"
-                        buttonType="link"
-                        size="lg"
-                        style={{ padding: 0 }}
-                    >
-                        Settings
-                    </Button> */}
+
                 </div>
             </CardHeader>
             <CardBody>
-                <form>
+                <form method="POST" >
                     <h6 className="text-purple-500 text-sm mt-3 mb-6 font-light uppercase">
                         User Information
                     </h6>
@@ -31,28 +90,45 @@ export default function SettingsForm() {
                             <Input
                                 type="text"
                                 color="purple"
+                                name="name"
                                 placeholder="Username"
+
+
+                                value={user.name}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
                             <Input
                                 type="email"
                                 color="purple"
+                                name="email"
                                 placeholder="Email Address"
+
+                                value={user.email}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
                             <Input
                                 type="text"
                                 color="purple"
+
                                 placeholder="First Name"
+                                name="firstname"
+                                value={user.firstname}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-6/12 pl-4 mb-10 font-light">
                             <Input
-                                type="email"
+                                type="text"
                                 color="purple"
+                                name="lastname"
                                 placeholder="Last Name"
+
+                                value={user.lastname}
+                                onChange={getUserData}
                             />
                         </div>
                     </div>
@@ -65,40 +141,78 @@ export default function SettingsForm() {
                             <Input
                                 type="text"
                                 color="purple"
+
                                 placeholder="Address"
+                                name="address"
+                                value={user.address}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-4/12 pr-4 mb-10 font-light">
                             <Input
                                 type="text"
                                 color="purple"
+
                                 placeholder="City"
+                                name="city"
+                                value={user.city}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-4/12 px-4 mb-10 font-light">
                             <Input
                                 type="text"
                                 color="purple"
+
                                 placeholder="Country"
+                                name="country"
+                                value={user.country}
+                                onChange={getUserData}
                             />
                         </div>
                         <div className="w-full lg:w-4/12 pl-4 mb-10 font-light">
                             <Input
                                 type="text"
                                 color="purple"
+
                                 placeholder="Postal Code"
+                                name="postalcode"
+                                value={user.postalcode}
+                                onChange={getUserData}
                             />
                         </div>
                     </div>
 
-                    <h6 className="text-purple-500 text-sm my-6 font-light uppercase">
+                    <h6 className="text-purple-500  text-sm my-6 font-light uppercase">
                         About Me
                     </h6>
                     <div className="flex flex-wrap mt-10 font-light">
-                        <Textarea color="purple" placeholder="About Me" />
+
+                        <Textarea color="purple"
+                            name="aboutme"
+                            value={user.aboutme}
+                            onChange={getUserData}
+                        />
+
                     </div>
                 </form>
+                <div className="my-10 mt-10">
+
+                    <Button
+                        color="purple"
+                        buttonType="filled"
+                        size="lg"
+                        rounded={false}
+                        block={false}
+                        iconOnly={false}
+                        ripple="dark"
+                        onClick={postData}
+                        type="submit"
+                    >
+                        Update
+                    </Button>
+                </div>
             </CardBody>
-        </Card>
+        </Card >
     );
 }
